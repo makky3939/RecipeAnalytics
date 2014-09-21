0c95c580b5569ee01903ca3c1fa4c2eab7c9eec6
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-09-21 10:52:03 +0900

    ModelのMethodを作成

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,27 +1,18 @@
 class IngredientsController < ApplicationController
   def par_count
-    @data = ''
-    # natto = Natto::MeCab.new
-    # @natto = natto.parse Ingredient.first.name
-    # natto.parse(text) do |n|
-    #   puts "#{n.surface}\t#{n.feature}"
-    # end
   end
 
   def par_count_json
-    items = {}
-    Ingredient.all.each do |item|
-      items[item.name] = 0 unless items[item.name]
-      items[item.name] += 1
+    ingredients = Ingredient.withFrequency
+    data = {
+      fields: ['item', 'size'],
+      values: []
+    }
+
+    ingredients.each do |key, val|
+      data[:values].push [key, val * 10] unless val == 1
     end
-    data =
-      {
-        "fields" => ["item","size"],
-        "values" => []
-      }
-    items.each do |key, val|
-      data["values"].push [key, val*10] unless val == 1
-    end
+
     render :json => data
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,15 @@
 class Ingredient < ActiveRecord::Base
   has_one :recipe, :class_name => "Recipe", :primary_key => 'recipe_id', :foreign_key => "recipe_id"
+
+  def self.withFrequency
+    ingredients = {}
+    Ingredient.all.each do |item|
+      if ingredients[item.name]
+        ingredients[item.name] += 1
+      else
+        ingredients[item.name] = 0
+      end
+    end
+    ingredients
+  end
 end
