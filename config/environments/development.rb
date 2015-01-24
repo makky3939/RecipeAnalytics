cf146dfe9597af7d9680123825dc49cc0dfdb604
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-01-25 02:00:17 +0900

    Assetsのログを非表示にするコードを削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.logger = Logger.new(config.paths["log"].first)
-  config.logger.formatter = Logger::FormatWithTime.new
-
   ENV["REDIS"] = "localhost:6379"
 end
